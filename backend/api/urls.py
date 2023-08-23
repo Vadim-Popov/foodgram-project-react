d004421ccd2ef55afebde74bae302e7edf46401d
Vadim <vadim.popov7277@yandex.ru>
2023-08-23 06:54:25 +0500

    fix_pep8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,8 +17,8 @@ router.register('recipes', RecipesViewSet)
 urlpatterns = [
      path('auth/token/login/', AuthToken.as_view(), name='login'),
      path('users/set_password/', set_password, name='set_password'),
-     path('users/<int:user_id>/subscribe/', AddAndDeleteSubscribe.as_view(),
-          name='subscribe'),
+     path('users/<int:user_id>/subscribe/',
+          AddAndDeleteSubscribe.as_view(), name='subscribe'),
      path('recipes/<int:recipe_id>/favorite/',
           AddDeleteFavoriteRecipe.as_view(), name='favorite_recipe'),
      path('recipes/<int:recipe_id>/shopping_cart/',
