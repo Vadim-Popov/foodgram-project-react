1a9aaa37ad0b12746d4ff894391f51d3c9d703a6
Vadim <vadim.popov7277@yandex.ru>
2023-08-23 06:21:15 +0500

    first_commit

---
+++ backend/users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+User = get_user_model()
+
+admin.site.unregister(User)  # Удалить регистрацию модели User
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'id', 'username', 'email',
+        'first_name', 'last_name', 'date_joined',)
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('date_joined', 'email', 'first_name')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(User, CustomUserAdmin)
